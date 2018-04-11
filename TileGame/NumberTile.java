128ef1be352f95f3a15ca23f7a83579c3ac41959
M. Chowdhury <mintychocolate101@gmail.com>
2018-04-11 14:19:13 -0700

    Update NumberTile.java

--- TileGame/NumberTile.java
+++ TileGame/NumberTile.java
@@ -1,14 +1,19 @@
 public class NumberTile {
-	int num1, num2, num3, num4;
+	private int num1, num2, num3, num4;
+	private boolean sorted;
 	
 	public NumberTile() {
+		//initializes tile with 4 random numbers
 		num1 = (int)(Math.random()*9) + 1;
 		num2 = (int)(Math.random()*9) + 1;
 		num3 = (int)(Math.random()*9) + 1;
 		num4 = (int)(Math.random()*9) + 1;
+		//indicates that tile is unsorted
+		sorted = false;
 	}
 	
 	public void rotate() {
+		//changes number assignments to simulate rotation
 		int temp = num1;
 		num1 = num2;
 		num2 = num3;
@@ -16,15 +21,27 @@ public class NumberTile {
 		num4 = temp;
 	}
 	
-	public int getLeft() {
-		return num2;
+	public int getTop() {
+		//returns top for alignment of tile
+		return num1;
 	}
 	
-	public int getRight() {
-		return num4;
+	public int getBottom() {
+		//returns bottom for alignment of tile
+		return num3;
+	}
+	
+	public boolean isSorted() {
+		//checks if tile is sorted or not
+		return sorted;
+	}
+	
+	public void sorted() {
+		//marks if tile has been sorted
+		sorted = true;
 	}
 	
 	public String toString() {
-		return "\t" + num1 + "\n" + num4 + "\t\t" + num2 + "\n\t" + num3;
+		return "\n\t" + num1 + "\n" + num4 + "\t\t" + num2 + "\n\t" + num3 + "\n";
 	}
 }
